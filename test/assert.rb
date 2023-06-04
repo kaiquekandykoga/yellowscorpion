30740cbb2fdcbbfc4cf30b226a6c8be26fe14f19
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2023-06-04 10:55:01 -0300

    add message

--- test/assert.rb
+++ test/assert.rb
@@ -17,3 +17,6 @@ end
 TestAssert.new.test_0 {
   printf "assert(#{_1}) \t=> "
 }
+
+  # Assertion failed: method test_0, file test/assert.rb, line 11.
+  # ruby 3.2.2 (2023-03-30 revision e51014f9c0) [x86_64-freebsd13.1]
