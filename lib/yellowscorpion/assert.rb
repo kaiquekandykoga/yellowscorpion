bd51ab8a21d6a692ed35a5a5efd907c4560508c4
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2023-06-04 10:46:03 -0300

    improve message

--- lib/yellowscorpion/assert.rb
+++ lib/yellowscorpion/assert.rb
@@ -3,8 +3,12 @@ module Yellowscorpion
     def assert(expression)
       return if expression == true
 
-      backlocation = caller_locations(1, 1)[0].to_s
-      printf "\e[31m#{backlocation}\e[0m \n"
+      backlocation = caller_locations(1, 1)[0]
+      lineno = backlocation.lineno
+      method_name = backlocation.to_s[/`.*'/][1..-2].split(" ")[-1]
+      filename = backlocation.to_s.split(":")[0]
+      message = "Assertion failed: method #{method_name}, file #{filename}, line #{lineno}."
+      printf "\e[31m#{message}\e[0m \n"
       abort
     end
   end
